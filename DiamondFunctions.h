ea2ceb35f1b6a22d616f228d765092030d00be0f
aless2003 <46018087+aless2003@users.noreply.github.com>
2019-04-23 23:46:42 +0200

    Add files via upload

---
+++ DiamondFunctions.h
@@ -0,0 +1,8 @@
+#ifndef COPYRIGHT_LIBRARY_H
+#define COPYRIGHT_LIBRARY_H
+
+#include "DiamondFunctions.c"
+
+void Copyright(void);
+void SpaceDelete(char[]);
+#endif
---
+++ cmake-build-debug/CMakeFiles/Copyright.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Copyright.
+# This may be replaced when dependencies are built.
